7e3ada3c547d929ab2820eb2f88a96aa88d77a34
Creewie <tabero2006@gmail.com>
2024-12-03 21:15:22 +0100

    Klikacz

--- app/src/main/java/com/example/moviebooktracker/MainActivity.kt
+++ app/src/main/java/com/example/moviebooktracker/MainActivity.kt
@@ -1,6 +1,13 @@
 package com.example.moviebooktracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,7 +23,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val title = findViewById<TextView>(R.id.title)
+        val addtitle = findViewById<EditText>(R.id.addtitle)
+        val addgenre = findViewById<EditText>(R.id.addgenre)
+        val add = findViewById<EditText>(R.id.addreview)
+        val addrating = findViewById<SeekBar>(R.id.addrating)
+        val addnew = findViewById<Button>(R.id.addnew)
+        val movieorbook = findViewById<RadioGroup>(R.id.movieorbook)
 
+        movieorbook.setOnCheckedChangeListener{_, isChecked ->
+            val radioButton = findViewById<RadioButton>(isChecked)
+            title.text = radioButton.text.toString()
+        }
+
+        addnew.setOnClickListener{_, ->
+            Toast.makeText(this, "Wypełnij dobrze wszystkie pola!", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
